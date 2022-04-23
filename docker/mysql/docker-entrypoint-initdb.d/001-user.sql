560c2dcb2cfa897da9422eb96f792235ebdbb547
nnnaoki1998 <naoki101130@icloud.com>
2022-04-23 10:26:28 +0900

    Add mysql in docker-compose.yml

---
+++ docker/mysql/docker-entrypoint-initdb.d/001-user.sql
@@ -0,0 +1,11 @@
+use spocon_db;
+
+create table user
+(
+  id int unsigned not null auto_increment,
+  name varchar(255) not null,
+  email varchar(255) not null unique,
+  created_at TIMESTAMP not null default CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  primary key (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
