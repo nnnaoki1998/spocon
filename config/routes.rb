a3aacc795740ed33b9a2ec7f27bb8ac56b241c39
田代昌之 <>
2022-11-30 00:21:21 +0900

    ユーザー情報の変更機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   post '/sport', to: 'sport#create'
   #　curl -X DELETE 127.0.0.1:3000/sport/3
   delete '/sport/:id', to: 'sport#destroy'
+  # curl -X PATCH -H "Content-Type: application/json" -d '{"name":"錦木千束","email":"chisato@lyco.com"}' 127.0.0.1:3000/user/1
+  get '/user/:id/edit', to: 'user#edit'
+  patch 'user/:id', to: 'user#update'
 end
