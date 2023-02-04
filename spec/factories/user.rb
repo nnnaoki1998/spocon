21b1b231d0d71b2eb4ff7ccf36b01fc6d8744a9a
田代昌之 <>
2023-02-04 14:36:26 +0900

    userのテストコード書きました

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+    factory :user do
+      name { Faker::Name.name }
+      email { Faker::Internet.free_email }
+      #password = Faker::Internet.password(min_length: 6)
+      #password { password }
+      #password_confirmation { password }
+    end
+  end
+  
