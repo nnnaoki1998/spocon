2c3c065c1bfc535c0e4b5ac903d436340819189a
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2023-03-19 19:16:17 +0900

    reactのバージョン18の書き方に合わせる

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Hello from './Hello';
 
-ReactDOM.render(
+const container = document.getElementById('root')
+
+if (!container) throw new Error('Failed to find the root element.')
+
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
     <Hello />
   </React.StrictMode>,
-  document.getElementById('app')
 );
