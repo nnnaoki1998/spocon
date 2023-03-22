945d8cf0bb7e615861cd3a9730f40bfd798120b3
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2023-03-23 00:00:43 +0900

    微修正

--- ui/src/components/PrivateRoute.tsx
+++ ui/src/components/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
-export const PrivateRoute: React.FC = ({children}) => {
+export const PrivateRoute: React.FC = ({ children }) => {
   const isAuthenticated = useAuth();
   return isAuthenticated ? <div>{children}</div> : <Navigate to="/signin" />;
 };
