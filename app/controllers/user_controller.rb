e445902ed04f3bc900973a30944c810173e974e2
田代昌之 <>
2023-05-15 21:59:11 +0900

    不要なメソッドの削除、100-insert.sql / routes.rbの修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,10 +12,6 @@ class UserController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
