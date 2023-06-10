f998b25ac6f59e8ffc89cbb53dc88e03602b9abb
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2023-06-10 21:08:59 +0900

    エントリーファイルの修正 - aws情報をルートファイルでインポート - React.StrictModeを削除（useEffectのレンダリングが2回行われるため）

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import Router from './Router';
+import { Amplify } from 'aws-amplify';
+import { Router } from './Router';
 import { ProvideAuth } from './hooks/useAuth';
+import awsExports from './aws-exports';
+
+Amplify.configure({ ...awsExports });
 
 const container = document.getElementById('root')
 
@@ -10,9 +14,7 @@ if (!container) throw new Error('Failed to find the root element.')
 const root = createRoot(container)
 
 root.render(
-  <React.StrictMode>
     <ProvideAuth>
       <Router />
     </ProvideAuth>
-  </React.StrictMode>,
 );
