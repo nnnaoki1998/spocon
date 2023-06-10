9bdcc70b02325378dc2618a251ffebe06ae73361
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2023-06-10 21:01:59 +0900

    外部のcssファイルを参照できるようにする

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -35,6 +35,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            }
+          }
+        ]
+      }
     ],
   },
   plugins: [
