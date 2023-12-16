757085461a0906ca7efdd7ce7dae2bd206865e4d
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2023-12-16 12:53:37 +0900

    Cognitoを再度作成し直して、S3を新規作成する

--- ui/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ ui/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,5 +4,22 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string"
     }
+  },
+  "auth": {
+    "spocon7d3e027d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "spoconS3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
