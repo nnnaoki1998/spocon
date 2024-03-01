05f4b06305d601533ed9459330d9febaccb7fc46
oirom <yamauchi.morio853@mail.kyutech.jp>
2024-03-02 01:47:16 +0900

    error fix and delete unnecessary file

--- frontend/src/AuthProvider.tsx
+++ frontend/src/AuthProvider.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { authContext, useProvideAuth } from "./hooks/auth/useAuth";
 
-export const AuthProvider: React.FC = ({ children }) => {
-    const auth = useProvideAuth();
-    return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+interface Props {
+  children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<Props> = ({ children }) => {
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 };
--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/auth/useAuth';
 
+interface Props {
+  children: React.ReactNode;
+}
+
 // ログイン後に表示する画面に使用する。
-export const PrivateRoute: React.FC = ({ children }) => {
+export const PrivateRoute: React.FC<Props> = ({ children }) => {
   const isAuthenticated = useAuth();
   return isAuthenticated ? <div>{children}</div> : <Navigate to="/" />;
 };
