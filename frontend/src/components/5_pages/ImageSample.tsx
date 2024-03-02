106f020fd45d91a27fbfd1b08e4dc0b4a8425ee0
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2024-03-02 15:31:49 +0900

    add page imageSample

---
+++ frontend/src/components/5_pages/ImageSample.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const ImageSample: React.FC = () => {
+  console.log('ImageSample'); // eslint-disable-line no-console
+  return <div>ImageSample</div>;
+};
+
+export { ImageSample };
