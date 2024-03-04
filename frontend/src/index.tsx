0221dd3e05035bdfe6e7d96e95047212722b0192
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2024-03-04 23:30:14 +0900

    update s3 access to protected. in case of private, another user cannot read.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,9 +7,9 @@ import awsExports from './aws-exports';
 
 Amplify.configure({ ...awsExports });
 
-// S3へのアクセスを各ユーザーが作成したオブジェクトのみに制限する
+// S3へのアクセスを「読み取りは全ユーザ許容、書き込みは認証ユーザのみ許容」の設定にする
 Storage.configure({
-  level: 'private',
+  level: 'protected',
 });
 
 const container = document.getElementById('root')
